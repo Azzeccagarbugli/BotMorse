35f57ba7d1ff954e43f7dacc145ce3652eaa169f
Azzeccagarbugli <f.coppola1998@gmail.com>
2017-03-10 00:54:05 +0100

    [UPDATE] Samuel Morse sarebbe fiero di tutto ciò

--- Morsetor.py
+++ Morsetor.py
@@ -4,6 +4,8 @@ import sys
 
 machine_state = 0
 
+# Crediti per la gestione della conversione Morse - Alphabet a https://gist.github.com/ebuckley/1842461
+
 morseAlphabet = {
     "A" : ".-",
     "B" : "-...",
@@ -31,7 +33,7 @@ morseAlphabet = {
     "X" : "-..-",
     "Y" : "-.--",
     "Z" : "--..",
-    " " : "/",
+    " " : "  ", # SPAZIO TRA DUE PAROLE
     "1" : ".----",
     "2" : "..---",
     "3" : "...--",
@@ -83,9 +85,13 @@ def handle(msg):
     content_type, chat_type, chat_id = telepot.glance(msg)
 
     global machine_state
+    str_testo_morse = ""
+    str_testo_alphabet = ""
 
     chat_id = msg['chat']['id']
     command_input = msg['text']
+
+    print(chat_id, content_type)
 
     if machine_state == 0 and content_type == 'text':
 
@@ -101,44 +107,45 @@ def handle(msg):
         if command_input == '/help' or command_input == '/help@FrazionetorBot':
 
             help_text = "Salve, puoi utilizzare il comando /atm per convertire un qualsiasi"
-            help_text += "messaggio in codice Morse mentre puoi utilizzare il comando /tam per convertire un codice Morse"
+            help_text += "messaggio in codice Morse mentre puoi utilizzare il comando /mta per convertire un codice Morse "
             help_text += "in un messaggio di testo.\nPuoi contattare lo sviluppatore su github.com/Azzeccagarbugli"
             bot.sendMessage(chat_id, help_text)
+
+            machine_state = 1
+
+        elif command_input == '/atm' or command_input == '/atm@FrazionetorBot':
+
+            morse_text = "Inserisci il messaggio che vuoi convertire in codice Morse"
+            bot.sendMessage(chat_id, morse_text)
 
             machine_state = 2
 
-    elif machine_state == 2 and content_type == 'text':
+        elif command_input == '/mta' or command_input == '/mta@FrazionetorBot':
 
-        if command_input == '/help' or command_input == '/help@FrazionetorBot':
-
-            help_text = "Salve, puoi utilizzare il comando /atm per convertire un qualsiasi"
-            help_text += "messaggio in codice Morse mentre puoi utilizzare il comando /mta per convertire un codice Morse"
-            help_text += "in un messaggio di testo.\nPuoi contattare lo sviluppatore su github.com/Azzeccagarbugli"
-            bot.sendMessage(chat_id, help_text)
+            alphabet_text = "Inserisci il codice Morse che vuoi convertire in un messagio"
+            bot.sendMessage(chat_id, alphabet_text)
 
             machine_state = 3
 
+    elif machine_state == 2 and content_type == 'text':
+
+        str_testo_morse = command_input.lower()
+        str_morse = encodeToMorse(str_testo_morse)
+        str_answer_morse = ("Il messaggio convertito in codice Morse è: {0}".format(str_morse))
+
+        bot.sendMessage(chat_id, str_answer_morse, parse_mode = "Markdown")
+        print(str_answer_morse)
+        machine_state = 1
+
     elif machine_state == 3 and content_type == 'text':
 
-        if command_input == '/atm' or command_input == '/atm@FrazionetorBot':
+        str_testo_alphabet = command_input.lower()
+        str_alphabet = decodeMorse(str_testo_alphabet, 0)
+        str_answer_alphabet = ("Il messaggio convertito in codice Morse è: {0}".format(str_alphabet))
 
-            str_testo = command_input.lower()
-            str_morse = encodeToMorse(str_testo)
-            str_answer_morse = ("_Il messaggio convertito in codice Morse è_: {0}".format(str_morse))
-
-            bot.sendMessage(chat_id, str_answer_morse, parse_mode = "Markdown")
-
-            machine_state = 1
-
-        elif command_input == '/mta' or command_input == '/mta@FrazionetorBot':
-
-            str_testo = command_input.lower()
-            str_alphabet = decodeMorse(str_testo, 0)
-            str_answer_alphabet = ("_Il messaggio convertito in codice Morse è_: {0}".format(str_alphabet))
-
-            bot.sendMessage(chat_id, str_answer_alphabet, parse_mode = "Markdown")
-
-            machine_state = 1
+        bot.sendMessage(chat_id, str_answer_alphabet, parse_mode = "Markdown")
+        print(str_answer_alphabet)
+        machine_state = 1
 
 bot = telepot.Bot('TOKEN')
 bot.message_loop(handle)
