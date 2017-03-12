1b404ad355264142e9d63f1f1278c91f5c931d61
Azzeccagarbugli <f.coppola1998@gmail.com>
2017-03-12 20:37:33 +0100

    [UPDATE] Morsetor ora utilizza un libreria mirabolante

--- Morsetor.py
+++ Morsetor.py
@@ -1,84 +1,9 @@
+import morse_talk as mtalk
 import telepot
 import time
 import sys
 
 machine_state = 0
-
-# Crediti per la gestione della conversione Morse - Alphabet a https://gist.github.com/ebuckley/1842461
-
-morseAlphabet = {
-    "A" : ".-",
-    "B" : "-...",
-    "C" : "-.-.",
-    "D" : "-..",
-    "E" : ".",
-    "F" : "..-.",
-    "G" : "--.",
-    "H" : "....",
-    "I" : "..",
-    "J" : ".---",
-    "K" : "-.-",
-    "L" : ".-..",
-    "M" : "--",
-    "N" : "-.",
-    "O" : "---",
-    "P" : ".--.",
-    "Q" : "--.-",
-    "R" : ".-.",
-    "S" : "...",
-    "T" : "-",
-    "U" : "..-",
-    "V" : "...-",
-    "W" : ".--",
-    "X" : "-..-",
-    "Y" : "-.--",
-    "Z" : "--..",
-    " " : "  ", # SPAZIO TRA DUE PAROLE
-    "1" : ".----",
-    "2" : "..---",
-    "3" : "...--",
-    "4" : "....-",
-    "5" : ".....",
-    "6" : "-....",
-    "7" : "--...",
-    "8" : "---..",
-    "9" : "----.",
-    "0" : "-----",
-    "." : ".-.-.-",
-    "," : "--..--",
-    ":" : "---...",
-    "?" : "..--..",
-    "'" : ".----.",
-    "-" : "-....-",
-    "/" : "-..-.",
-    "@" : ".--.-.",
-    "=" : "-...-"
-    }
-
-inverseMorseAlphabet=dict((v,k) for (k,v) in morseAlphabet.items())
-
-# Codifica un messaggio da Morse in alfabeto
-def decodeMorse(code, positionInString = 0):
-
-    if positionInString < len(code):
-        morseLetter = ""
-        for key,char in enumerate(code[positionInString:]):
-            if char == " ":
-                positionInString = key + positionInString + 1
-                letter = inverseMorseAlphabet[morseLetter]
-                return letter + decodeMorse(code, positionInString)
-            else:
-                morseLetter += char
-    else:
-        return ""
-
-# Codifca un messaggio in codice Morse, lo spazio è rapressentato dal simbolo '/'
-def encodeToMorse(message):
-    encodedMessage = ""
-    for char in message[:]:
-        encodedMessage += morseAlphabet[char.upper()] + " "
-
-    return encodedMessage
 
 # Funzione che viene eseguita all'arrivo di ogni nuovo messaggio
 def handle(msg):
@@ -106,7 +31,7 @@ def handle(msg):
 
         if command_input == '/help' or command_input == '/help@FrazionetorBot':
 
-            help_text = "Salve, puoi utilizzare il comando /atm per convertire un qualsiasi"
+            help_text = "Salve, puoi inizare a utilizzare il comando /atm per convertire un qualsiasi"
             help_text += "messaggio in codice Morse mentre puoi utilizzare il comando /mta per convertire un codice Morse "
             help_text += "in un messaggio di testo.\nPuoi contattare lo sviluppatore su github.com/Azzeccagarbugli"
             bot.sendMessage(chat_id, help_text)
@@ -130,7 +55,7 @@ def handle(msg):
     elif machine_state == 2 and content_type == 'text':
 
         str_testo_morse = command_input.lower()
-        str_morse = encodeToMorse(str_testo_morse)
+        str_morse = mtalk.encode(str_testo_morse)
         str_answer_morse = ("Il messaggio convertito in codice Morse è: {0}".format(str_morse))
 
         bot.sendMessage(chat_id, str_answer_morse, parse_mode = "Markdown")
@@ -139,8 +64,8 @@ def handle(msg):
 
     elif machine_state == 3 and content_type == 'text':
 
-        str_testo_alphabet = command_input.lower()
-        str_alphabet = decodeMorse(str_testo_alphabet, 0)
+        str_testo_alphabet = str(command_input.lower())
+        str_alphabet = mtalk.decode(str_testo_alphabet)
         str_answer_alphabet = ("Il messaggio convertito in codice Morse è: {0}".format(str_alphabet))
 
         bot.sendMessage(chat_id, str_answer_alphabet, parse_mode = "Markdown")
